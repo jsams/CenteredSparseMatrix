22ec7ec56ad1049a2a19c77229724a4bdadb5ced
James Sams <sams.james@gmail.com>
2017-10-02 20:38:03 -0700

    some explicit in place tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,11 +8,11 @@ using Base.Test
 
 # Just some setup
 
-n = 100
-m = 50
-d = 0.3
+n = 5
+m = 3
+d = 0.8
 
-k = 30
+k = 2
 
 A = sprand(n, m, d);
 Acd = full(A .- mean(A, 1));
@@ -24,8 +24,15 @@ X = rand(m, k);
 y = rand(n);
 Y = rand(n, k);
 
+z = zeros(size(Acs, 1));
+Z = zeros(size(Acs, 1), size(X, 2));
+# the actual tests
+
 @test isapprox(Acd * x, Acs * x)
+@test isapprox(Acd * x, A_mul_B!(z, Acs, x))
 @test isapprox(Acd * X, Acs * X)
+@test isapprox(Acd * X, A_mul_B!(Z, Acs, X))
+
 @test isapprox(Acd' * y, Ac_mul_B(Acs, y))
 @test isapprox(Acd' * y, Acs' * y)
 @test isapprox(Acd' * y, Acs'y)
